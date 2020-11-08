1360b7ab4e32427234ee0e6889986a25612ed814
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-08 14:54:42 +0000

    Completed Scrabble game

--- scrabble.py
+++ scrabble.py
@@ -15,6 +15,36 @@ class Scrabble:
 # Create child class called calculator
 class Calculator(Scrabble):
 
-    def __init__(self, word): # Initialize the class
+    def __init__(self, word):  # Initialize the class
         super().__init__()  # Inherit the attributes from parent class
+        self.score = 0  # Set the score to 0 to start off with
+        self.word = word.upper()  # .upper() method saves us from having to check if word is capitalised or lower case
+
+    def check_score(self):
+        # Create loop that creates a score for the "word" argument that is passed through
+        for letter in self.word:
+            if letter in self.one:
+                self.score += 1
+            elif letter in self.two:
+                self.score += 2
+            elif letter in self.three:
+                self.score += 3
+            elif letter in self.four:
+                self.score += 4
+            elif letter in self.five:
+                self.score += 5
+            elif letter in self.eight:
+                self.score += 8
+            elif letter in self.ten:
+                self.score += 10
+            else:
+                # Return a logical message if a character isn't in any of the lists
+                return "Sorry, one of your letters do not exist, try again "
+
+        return self.score  # Return the score
+
+
+# Create an object to assign the class to
+test = Calculator("Basketball")
+print(test.check_score())
 
